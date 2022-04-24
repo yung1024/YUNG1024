09c55a70fcff841cf671ce2e364e65f7daf9d7e8
yung1024 <104044178+yung1024@users.noreply.github.com>
2022-04-24 11:32:01 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+//这个是备注
+/*这个也是备注*/
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+myButton.onclick = function() {
+   setUserName();
+}
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla 酷毙了，' + myName;
+  }
+}
